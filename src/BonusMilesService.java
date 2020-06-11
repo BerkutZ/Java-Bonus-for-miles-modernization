24b54e0ff512207d10f90714f6afe5520c4dca5e
Ruslan Badertdinov <badertdinovrr@yandex.ru>
2020-06-11 11:35:09 +0500

    "Something unimportant changes"

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int bonus_for_miles = 1;
-        int calculate = cost / 20 * bonus_for_miles;
+        int bonusForMiles = 1;
+        int calculate = cost / 20 * bonusForMiles;
         return calculate;
     }
 
